3c73ee4406fb96e3b6b346c22cfa6b6f2b8a1b4d
Cabe Branson <cbranson@webmd.net>
2015-05-05 17:24:33 -0700

    refactor

--- werk.js
+++ werk.js
@@ -5,8 +5,10 @@
 
   function makeModal(werk) {
 
+    // Create the tab container and close button
     var tabClass = 'werk__tabs'
-    ,   closeClass = 'werk__close';
+    ,   closeClass = 'werk__close'
+    ,   activeClass = 'is-active';
 
     werk.appendChild(document.createElement('ul')).className = tabClass;
     werk.appendChild(document.createElement('div')).className = closeClass;
@@ -14,25 +16,28 @@
     var tabHolder = werk.querySelector('.' + tabClass)
     ,   tabItems = tabHolder.children
     ,   tabCount = werk.querySelectorAll('img').length
-    ,   activeClass = 'is-active'
-    ,   elem, i;
+    ,   img = werk.querySelector('img')
+    ,   closeBtn = werk.querySelector('.' + closeClass)
+    ,   el, i;
 
+    // Make a new tab for each image
     for (i = 0; i < tabCount; i++) {
-      elem = document.createElement('li');
-      tabHolder.appendChild(elem);
-      elem.setAttribute('data-index', i);
-      elem.innerHTML = i + 1;
-      elem.addEventListener('click', tabClick);
+      el = document.createElement('li');
+      tabHolder.appendChild(el);
+      el.setAttribute('data-index', i);
+      el.innerHTML = i + 1;
+
+      // Attach event
+      el.addEventListener('click', tabClick);
     }
 
-    var img = werk.querySelector('img');
-      img.setAttribute('src', img.getAttribute('data-src'));
-      img.removeAttribute('data-src');
-      img.classList.add(activeClass);
-      tabItems[0].classList.add(activeClass);
+    // Set the first imag eand tab as active
+    img.setAttribute('src', img.getAttribute('data-src'));
+    img.removeAttribute('data-src');
+    img.classList.add(activeClass);
+    tabItems[0].classList.add(activeClass);
 
-      var closeBtn = werk.querySelector('.' + closeClass);
-
+    // Tidy up the close button
     closeBtn.innerHTML = 'CLOSE';
     closeBtn.addEventListener('click', function() {
       werk.classList.remove('is-visible');
@@ -40,28 +45,32 @@
     });
 
     function tabClick(e) {
+
       var index = e.target.getAttribute('data-index')
       ,   imageCollection = werk.querySelectorAll('img')
-      ,   elem, j;
+      ,   el, i;
 
-      if (tabItems[index].classname = activeClass) {
-        for (j = 0; j < imageCollection.length; j++) {
-          elem = imageCollection[j];
-          if (j == index) {
-            if (elem.hasAttribute('data-src')) {
-              elem.setAttribute('src', elem.getAttribute('data-src'));
-              elem.removeAttribute('data-src');
+      if ((tabItems[index].classname = activeClass)) {
+        for (i = 0; i < imageCollection.length; i++) {
+          el = imageCollection[i];
+
+          if (i == index) {
+            if (el.hasAttribute('data-src')) {
+              el.setAttribute('src', el.getAttribute('data-src'));
+              el.removeAttribute('data-src');
             }
-            elem.classList.add(activeClass);
-            tabItems[j].classList.add(activeClass);
+
+            el.classList.add(activeClass);
+            tabItems[index].classList.add(activeClass);
+
           } else {
-            tabItems[j].classList.remove(activeClass);
-            elem.classList.remove(activeClass);
+            tabItems[i].classList.remove(activeClass);
+            el.classList.remove(activeClass);
           }
         }
       }
     }
-  };
+  }
 
   function showModal(el) {
     el.addEventListener('click', function() {
