0e739b0b0e254c329f19ccfb72c1fcf569bd56e9
Cabe Branson <caleb.branson@gmail.com>
2015-08-17 11:36:40 -0700

    automate tests

--- gulpfile.js
+++ gulpfile.js
@@ -2,30 +2,36 @@ var gulp = require('gulp');
 var rename = require("gulp-rename");
 var postcss = require('gulp-postcss');
 var autoprefixer = require('autoprefixer');
-var pixrem = require('gulp-pixrem');
-var jshint = require('gulp-jshint');
+var pixrem = require('pixrem');
+var cssmin = require('gulp-cssmin');
 var uglify = require("gulp-uglify");
-
+var jshint = require('gulp-jshint');
+var mochaPhantomJS = require("gulp-mocha-phantomjs");
 
 gulp.task('css', function () {
   var processors = [
-    autoprefixer({browsers: ['last 1 version']})
+    autoprefixer({browsers: ['last 1 version']}),
+    pixrem()
   ];
   return gulp.src('./src/bokeh.css')
     .pipe(postcss(processors))
-    .pipe(pixrem())
+    .pipe(cssmin())
+    .pipe(rename({suffix: '.min'}))
     .pipe(gulp.dest('./dist'));
 });
 
 gulp.task('js', function() {
-  return gulp.src('./src/*.js')
+  return gulp.src('./src/bokeh.js')
     .pipe(jshint({laxcomma:true}))
     .pipe(jshint.reporter('default'))
+    .pipe(uglify())
+    .pipe(rename({suffix: '.min'}))
     .pipe(gulp.dest('./dist'));
 });
 
-// gulp.task('min', function() {
-//   return gulp.src('/dist/bokeh.js')
-//   .pipe(uglify())
-//   .pipe(rename('/bokeh.min.js'))
-// })
+gulp.task('test', function () {
+  return gulp.src('test/index.htm')
+  .pipe(mochaPhantomJS());
+});
+
+gulp.task('default', ['css', 'js', 'test'])
--- test/test.js
+++ test/test.js
@@ -6,71 +6,93 @@ function $$(e) {
   return document.querySelectorAll(e);
 }
 
+// Appease PhantomJS
+function click(el){
+    var ev = document.createEvent("MouseEvent");
+    ev.initMouseEvent(
+        "click",
+        true /* bubble */, true /* cancelable */,
+        window, null,
+        0, 0, 0, 0, /* coordinates */
+        false, false, false, false, /* modifier keys */
+        0 /*left*/, null
+    );
+    el.dispatchEvent(ev);
+}
+
 var should  = chai.should()
 ,   MODAL   = document.getElementById('test')
 ,   TABLIST = $('.bokeh__tabs')
 ,   TABS    = $$('.bokeh__tabs li')
 ,   BLUR    = $('.bokeh-blur')
-,   i       = TABS.length
 ,   IMGS    = $$('img')
 ,   CLOSE   = $('.bokeh__close')
 ,   SRC     = $('img').hasAttribute('src')
 ,   DATASRC = $('img').hasAttribute('data-src')
 ,   LINK    = $('.bokeh-link')
-,   ACTIVE  = 'is-active';
+,   ACTIVE  = 'is-active'
+,   t       = TABS.length
+;
 
-describe('DOM', function(){
-    it('created tab list', function() {
-        TABLIST.should.not.equal(null);
-    })
+describe('DOM', function() {
+  it('created tabs container', function() {
+    TABLIST.should.not.equal(null);
+  })
 
-    it('created tabs for each image', function() {
-        TABS.length.should.equal(IMGS.length);
-    })
+  it('created tabs for each image', function() {
+    t.should.equal(IMGS.length);
+  })
 
-    it('created close button', function() {
-        CLOSE.should.not.equal(null);
-    })
+  it('created close button', function() {
+    CLOSE.should.not.equal(null);
+  })
 
-    it('data-src -> src', function() {
-        SRC.should.equal(true);
-        DATASRC.should.equal(false);
-    })
+  it('data-src => src', function() {
+    SRC.should.equal(true);
+    DATASRC.should.equal(false);
+  })
 
-    it('tabs have data-index attribute', function() {
-        for (var i = 0; i < TABS.length; i++) {
-            TABS[i].hasAttribute('data-index').should.equal(true);
-        };
-    })
+  it('tabs have data-index attr', function() {
+    for (var i = 0; i < t; i++) {
+      TABS[i].hasAttribute('data-index').should.equal(true);
+    };
+  })
 })
 
-describe('Events', function(){
+describe('Events', function() {
 
-    it('opened successfully', function () {
-         LINK.click();
-         MODAL.className.should.equal('bokeh is-visible');
-         BLUR.className.should.equal('bokeh-blur has-blur')
-         TABS[0].className.should.equal(ACTIVE);
-         IMGS[0].className.should.equal(ACTIVE);
-    })
+  it('opened', function() {
+    click(LINK);
+    MODAL.className.should.equal('bokeh is-visible');
+    TABS[0].className.should.equal(ACTIVE);
+    IMGS[0].className.should.equal(ACTIVE);
+  })
 
-    it('clicked second tab', function () {
-         TABS[1].click();
-         TABS[1].className.should.equal(ACTIVE);
-         TABS[0].className.should.not.equal(ACTIVE);
-         IMGS[0].className.should.not.equal(ACTIVE);
-         IMGS[1].className.should.equal(ACTIVE);
-    })
+  it('blur on', function() {
+    BLUR.className.should.equal('bokeh-blur has-blur')
+  })
 
-    it('clicked last tab', function () {
-         TABS[i - 1].click();
-         TABS[i - 1].className.should.equal(ACTIVE);
-         IMGS[i - 1].className.should.equal(ACTIVE);
-         TABS[0].className.should.not.equal(ACTIVE);
-    })
+  it('clicked second tab', function() {
+    click(TABS[1]);
+    TABS[1].className.should.equal(ACTIVE);
+    TABS[0].className.should.not.equal(ACTIVE);
+    IMGS[0].className.should.not.equal(ACTIVE);
+    IMGS[1].className.should.equal(ACTIVE);
+  })
 
-    it('closed successfully', function () {
-         CLOSE.click();
-         MODAL.className.should.equal('bokeh');
-    })
+  it('clicked last tab', function() {
+    click(TABS[t - 1]);
+    TABS[t - 1].className.should.equal(ACTIVE);
+    IMGS[t - 1].className.should.equal(ACTIVE);
+    TABS[0].className.should.not.equal(ACTIVE);
+  })
+
+  it('closed', function() {
+    click(CLOSE);
+    MODAL.className.should.equal('bokeh');
+  })
+
+  it('blur off', function() {
+    BLUR.className.should.equal('bokeh-blur')
+  })
 })
